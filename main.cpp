597237bb11f7fb63baf516d6469972a44abe21c6
omasher <ashermcoa@gmail.com>
2023-10-02 13:08:42 +0300

    Updated g++ version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-
-int main()
-{
-    return 0;
+#include <string>
+#include <string_view>
+#include <format>
+ 
+template <typename... Args>
+std::string dyna_print(std::string_view rt_fmt_str, Args&&... args) {
+    return std::vformat(rt_fmt_str, std::make_format_args(args...));
+}
+ 
+int main() {
+    std::cout << std::format("Hello {}!\n", "world");
+ 
+    std::string fmt;
+    for (int i{}; i != 3; ++i) {
+        fmt += "{} "; // constructs the formatting string
+        std::cout << fmt << " : ";
+        std::cout << dyna_print(fmt, "alpha", 'Z', 3.14, "unused");
+        std::cout << '\n';
+    }
 }
